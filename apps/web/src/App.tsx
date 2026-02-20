9abbaae95d13752091951ba7bbd1766ca01a6879
Himank Sharma <yourgithubemail@example.com>
2026-02-20 15:39:20 +0530

    v2

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      APP WORKING
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/web/src/pages/Login.tsx
@@ -0,0 +1,4 @@
+export default function Login() {
+    return <div className="p-4 text-xl">Home Page</div>;
+  }
+  
